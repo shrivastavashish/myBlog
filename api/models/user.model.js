8db6d16174f852aba2251072502f9548f03457fc
shrivastavashish <shrivastav.ashish@outlook.com>
2024-03-01 15:44:49 +0530

    create user model

---
+++ api/models/user.model.js
@@ -0,0 +1,29 @@
+import mongoose from 'mongoose';
+
+const userSchema = new mongoose.Schema({
+    username: {
+        type: String,
+        required: true,
+        unique: true,
+        minlength: 3,
+        maxlength: 50
+    },
+    email: {
+        type: String,
+        required: true,
+        minlength: 5,
+        maxlength: 255,
+        unique: true
+    },
+    password: {
+        type: String,
+        required: true,
+        minlength: 5,
+        maxlength: 1024
+    },
+    }, { timestamps: true }
+    );
+
+    const User = mongoose.model('User', userSchema);
+
+    export default User;
