323d8e65d113b686b43d242fc28223a205afaf4f
shrivastavashish <shrivastav.ashish@outlook.com>
2024-03-15 18:43:50 +0530

    added user api update route

--- api/controllers/user.controller.js
+++ api/controllers/user.controller.js
@@ -1,3 +1,58 @@
+import bcryptjs from "bcryptjs";
+import { errorHandler } from "../utils/erroor.js";
+import User from "../models/user.model.js";
 export const test = (req, res) => {
-    res.json({message: 'API is working fine!'});
+  res.json({ message: "API is working fine!" });
+};
+
+export const updateUser = async (req, res, next) => {
+  if (req.user._id !== req.params.userId) {
+    return next(errorHandler(403, "Forbidden"));
+  }
+  if (req.body.password) {
+    if (req.body.password.length < 8) {
+      return next(
+        errorHandler(400, "Password must be at least 8 characters long")
+      );
+    }
+    req.body.password = bcryptjs.hashSync(req.body.password, 10);
+  }
+  if (req.body.username) {
+    if (req.body.username.length < 5 || req.body.username.length > 20) {
+      return next(
+        errorHandler(400, "Username must be at least 5  and 20 characters long")
+      );
+    }
+    if (req.body.username.includes(" ")) {
+      return next(errorHandler(400, "Username cannot contain spaces"));
+    }
+    if (req.body.username !== req.body.username.toLowerCase()) {
+      return next(errorHandler(400, "Forbidden Username must be lowercase"));
+    }
+    if (!req.body.username.match(/^[a-z0-9]+$/)) {
+      return next(
+        errorHandler(
+          400,
+          "Username can only contain lowercase letters and numbers"
+        )
+      );
+    }
+    try {
+      const updateUser = await User.findByIdAndUpdate(
+        req.params.userId,
+        {
+          $set: {
+            username: req.body.username,
+            email: req.body.email,
+            password: req.body.password,
+          },
+        },
+        { new: true }
+      );
+      const { password, ...rest } = updateUser._doc;
+      res.status(200).json(rest);
+    } catch (error) {
+      next(error);
+    }
+  }
 };
--- api/index.js
+++ api/index.js
@@ -1,30 +1,34 @@
-import express from 'express';
-import mongoose from 'mongoose';
-import dotenv from 'dotenv';
-import userRoutes from './routes/user.route.js';
-import authRoutes from './routes/auth.route.js';
+import express from "express";
+import mongoose from "mongoose";
+import dotenv from "dotenv";
+import userRoutes from "./routes/user.route.js";
+import authRoutes from "./routes/auth.route.js";
+import cookieParser from "cookie-parser";
 
 dotenv.config();
 
-mongoose.connect(process.env.MONGO)
-.then(() => {
-        console.log('Connected to MongoDB database');
-    }).catch(err => {
-        console.log('Error connecting to database', err);
-    });
+mongoose
+  .connect(process.env.MONGO)
+  .then(() => {
+    console.log("Connected to MongoDB database");
+  })
+  .catch((err) => {
+    console.log("Error connecting to database", err);
+  });
 
 const app = express();
 app.use(express.json());
+app.use(cookieParser());
 
 app.listen(3000, () => {
-  console.log('Server is running on port 3000');
+  console.log("Server is running on port 3000");
 });
 
-app.use('/api/user', userRoutes);
-app.use('/api/auth', authRoutes);
+app.use("/api/user", userRoutes);
+app.use("/api/auth", authRoutes);
 
 app.use((err, req, res, next) => {
-    const status = err.status || 500;
-    const message = err.message || 'Internal Server Error';
-    res.status(status).json({success: false, statusCode: status, message});
+  const status = err.status || 500;
+  const message = err.message || "Internal Server Error";
+  res.status(status).json({ success: false, statusCode: status, message });
 });
