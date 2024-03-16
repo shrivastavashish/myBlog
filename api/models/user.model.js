2db91008e840a493943a7c025995973a7eae0748
shrivastavashish <shrivastav.ashish@outlook.com>
2024-03-16 17:42:45 +0530

    add admin function to the user

--- api/models/user.model.js
+++ api/models/user.model.js
@@ -1,35 +1,40 @@
-import mongoose from 'mongoose';
+import mongoose from "mongoose";
 
-const userSchema = new mongoose.Schema({
+const userSchema = new mongoose.Schema(
+  {
     username: {
-        type: String,
-        required: true,
-        unique: true,
-        minlength: 3,
-        maxlength: 50
+      type: String,
+      required: true,
+      unique: true,
+      minlength: 3,
+      maxlength: 50,
     },
     email: {
-        type: String,
-        required: true,
-        minlength: 5,
-        maxlength: 255,
-        unique: true
+      type: String,
+      required: true,
+      minlength: 5,
+      maxlength: 255,
+      unique: true,
     },
     password: {
-        type: String,
-        required: true,
-        minlength: 5,
-        maxlength: 1024
+      type: String,
+      required: true,
+      minlength: 5,
+      maxlength: 1024,
     },
     profilePicture: {
-        type: String,
-        default: 'https://img.freepik.com/free-psd/3d-illustration-person-with-sunglasses_23-2149436188.jpg?size=626&ext=jpg'
+      type: String,
+      default:
+        "https://img.freepik.com/free-psd/3d-illustration-person-with-sunglasses_23-2149436188.jpg?size=626&ext=jpg",
     },
+    isAdmin: {
+      type: Boolean,
+      default: false,
+    },
+  },
+  { timestamps: true }
+);
 
+const User = mongoose.model("User", userSchema);
 
-    }, { timestamps: true }
-    );
-
-    const User = mongoose.model('User', userSchema);
-
-    export default User;
+export default User;
